245f26604ca0e9f717f0734719de0f9a2a3e0c2e
Juan <pablogomez.nov@gmail.com>
2022-01-02 13:44:14 -0300

    Agregue un footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import TodoList from "./components/TodoList"
 const App = () => {
 
   return (
-    <div className="container w-75 p-3 mt-5 app--todo">
+    <div className="container p-3 mt-5 app--todo">
       <h1>App de tareas</h1>
       <TodoList/>
     </div>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+    return (
+        <footer className="bg-dark fixed-bottom">
+            <div className="app--footer">
+                <a href="https://www.instagram.com" target="_blank">
+                    <i className="fab fa-instagram"></i>
+                    Instagram
+                </a>
+                <a href="https://github.com/GuserHub23" target="_blank">
+                    <i className="fab fa-github"></i>
+                    GitHub
+                </a>
+                <a href="https://www.linkedin.com/in/juan-pablo-g%C3%B3mez-837a3321a/" target="_blank">
+                    <i className="fab fa-linkedin"></i>
+                    linked In
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
